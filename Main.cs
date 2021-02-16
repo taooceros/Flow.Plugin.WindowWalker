4c1cfe28666f4e742d41b71e34df91c0cb78b2ed
Jeremy Wu <jeremy24wu@gmail.com>
2021-02-16 22:32:43 +1100

    remove empty title entries

--- Main.cs
+++ Main.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation
+﻿// Copyright (c) Microsoft Corporation
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -68,6 +68,7 @@ namespace Microsoft.Plugin.WindowWalker
 
             var results =
                     searchResults
+                    .Where(x => !string.IsNullOrEmpty(x.Result.Title))
                     .Select(x =>
                         new Result()
                         {

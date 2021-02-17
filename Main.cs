5fc70ad615a8fa48f13b1ac0c277f01b4d1f69d5
弘韬 张 <hongtao_zhang@outlook.com>
2021-02-18 06:54:15 +0800

    add highlight data for result

--- Main.cs
+++ Main.cs
@@ -73,6 +73,7 @@ namespace Microsoft.Plugin.WindowWalker
                               Title = x.Result.Title,
                               IcoPath = IconPath,
                               Score = x.Score,
+                              TitleHighlightData = x.SearchMatchesInTitle.MatchData,
                               SubTitle = $"{Properties.Resources.wox_plugin_windowwalker_running} : {x.Result.ProcessName}",
                               ContextData = x.Result,
                               Action = c =>

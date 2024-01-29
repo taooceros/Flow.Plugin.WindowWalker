19f213ef63e859aa9cc13e0292385c26b3009a18
Hongtao Zhang <hongtao_zhang@outlook.com>
2024-01-29 10:44:19 -0600

    do not assign IVirtualDesktopManagerInternal if the windows version is not new enough

--- Components/VirtualDesktopManager.cs
+++ Components/VirtualDesktopManager.cs
@@ -1,15 +1,15 @@
-﻿#nullable disable
-using System;
+﻿using System;
+using System.Diagnostics;
 using Flow.Plugin.WindowWalker.Components.COM;
 
 namespace Flow.Plugin.WindowWalker.Components
 {
     public static class DesktopManager
     {
-        internal static IVirtualDesktopManagerInternal VirtualDesktopManagerInternal;
-        internal static IVirtualDesktopManager VirtualDesktopManager;
-        internal static IVirtualDesktopPinnedApps VirtualDesktopPinnedApps;
-        public static IApplicationViewCollection ApplicationViewCollection;
+        internal static IVirtualDesktopManagerInternal? VirtualDesktopManagerInternal;
+        internal static IVirtualDesktopManager? VirtualDesktopManager;
+        internal static IVirtualDesktopPinnedApps? VirtualDesktopPinnedApps;
+        public static IApplicationViewCollection? ApplicationViewCollection;
 
         static DesktopManager()
         {
@@ -18,26 +18,34 @@ namespace Flow.Plugin.WindowWalker.Components
 
         private static void Init()
         {
-            var shell = (IServiceProvider10)Activator.CreateInstance(Type.GetTypeFromCLSID(Guids.CLSID_ImmersiveShell));
+            var shell = (IServiceProvider10?)Activator.CreateInstance(
+                Type.GetTypeFromCLSID(Guids.CLSID_ImmersiveShell)!);
 
-            VirtualDesktopManager = (IVirtualDesktopManager)Activator.CreateInstance(
-                Type.GetTypeFromCLSID(Guids.CLSID_VirtualDesktopManager));
+            VirtualDesktopManager = (IVirtualDesktopManager?)Activator.CreateInstance(
+                Type.GetTypeFromCLSID(Guids.CLSID_VirtualDesktopManager)!);
 
-            VirtualDesktopManagerInternal = (IVirtualDesktopManagerInternal)shell.QueryService(
-                Guids.CLSID_VirtualDesktopManagerInternal,
-                typeof(IVirtualDesktopManagerInternal).GUID);
-            ApplicationViewCollection = (IApplicationViewCollection)shell.QueryService(
+            InitializeVirtualDesktopManagerInternal(shell);
+
+            ApplicationViewCollection = (IApplicationViewCollection?)shell?.QueryService(
                 typeof(IApplicationViewCollection).GUID,
                 typeof(IApplicationViewCollection).GUID);
-            VirtualDesktopPinnedApps = (IVirtualDesktopPinnedApps)shell.QueryService(
+            VirtualDesktopPinnedApps = (IVirtualDesktopPinnedApps?)shell?.QueryService(
                 Guids.CLSID_VirtualDesktopPinnedApps,
                 typeof(IVirtualDesktopPinnedApps).GUID);
+        }
+
+        private static void InitializeVirtualDesktopManagerInternal(IServiceProvider10? shell)
+        {
+            if (Environment.OSVersion.Version.MinorRevision > 23000)
+                VirtualDesktopManagerInternal = (IVirtualDesktopManagerInternal?)shell?.QueryService(
+                    Guids.CLSID_VirtualDesktopManagerInternal,
+                    typeof(IVirtualDesktopManagerInternal).GUID);
         }
 
         internal static IApplicationView GetApplicationView(this IntPtr hWnd)
         {
             // get application view to window handle
-            ApplicationViewCollection.GetViewForHWnd(hWnd, out var view);
+            ApplicationViewCollection!.GetViewForHWnd(hWnd, out var view);
             return view;
         }
     }
